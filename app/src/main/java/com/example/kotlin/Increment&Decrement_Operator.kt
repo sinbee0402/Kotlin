754db81ed0c20fc9d5ef02c32fd22c65bbd09ce8
sinbee0402 <osb3476@gmail.com>
2021-06-21 08:08:07 +0900

    [add] Increment&Decrement_Operator.kt

---
+++ app/src/main/java/com/example/kotlin/Increment&Decrement_Operator.kt
@@ -0,0 +1,14 @@
+package com.example.kotlin
+
+// 6.Increment & Decrement Operator
+
+fun main(args: Array<String>): Unit {
+    var a = 10
+    var b = 5
+
+    println(a++ +b)
+    println(a)
+    println(--b)
+}
+
+//Next: Bitwise Operator
