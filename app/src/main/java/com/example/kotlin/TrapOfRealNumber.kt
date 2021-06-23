0a0c35b50eafaac9a7d0ae14b4f6891c02cb1965
sinbee0402 <osb3476@gmail.com>
2021-06-23 23:59:55 +0900

    [add] TrapOfRealNumber.kt

---
+++ app/src/main/java/com/example/kotlin/TrapOfRealNumber.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin
+
+// 9. Trap of RealNumber
+
+fun main(args: Array<String>): Unit{
+    println(0.1f+0.1f+0.1f)
+    println(0.1f+0.1f+0.1f+0.1f+0.1f+0.1f+0.1f+0.1f+0.1f+0.1f)
+    println(0.1f+10)
+}
+
+//Next:
