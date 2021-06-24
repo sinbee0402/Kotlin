46e14e79e86398d9f481a50f0a2ea924f67a566c
sinbee0402 <osb3476@gmail.com>
2021-06-25 01:21:49 +0900

    [add] StringType.kt

---
+++ app/src/main/java/com/example/kotlin/StringType.kt
@@ -0,0 +1,20 @@
+package com.example.kotlin
+
+// 11. StringType
+
+fun main(args: Array<String>): Unit{
+//문자열: 문자(Char)들을 일렬로 나열한 것
+
+    var str:String="Hello"
+    println(str)
+
+    str=str+"\nKotlin!"
+    println(str)
+
+    println(str[8])
+
+    val num=10*5+3
+    println(str+num)
+}
+
+//Next: Expression Into String
